c9e9bed23ff4cbc8b403b2087372729f07009f14
Ilalande <jcasier@protonmail.com>
2022-04-08 17:27:34 +0200

    Adding style with style component. Adding ccomponents header, footer, suggerator, TypeCarroussel, CarrousselNews to structure the site

---
+++ frontend/src/components/Suggerator/index.jsx
@@ -0,0 +1,9 @@
+import SSuggerator from "./style";
+
+export default function Suggerator() {
+  return (
+    <SSuggerator>
+      <p>Ici le suggerator</p>
+    </SSuggerator>
+  );
+}
---
+++ frontend/src/style.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export default styled.div`
+  * {
+    box-sizing: border-box;
+  }
+
+  /* 
+
+  Tentative de déclaration des variables : 
+  variable declaration GENERALE
+  :root {
+   
+    --color-bg-general: blue;
+    --font-size-general: 1rem;
+    --color-font-general: blue;
+    --font-weight-general: 400;
+    --font-general: "Andika New Basic", sans-serif;
+    --line-height-general: 1.75;
+
+
+    --font-size-h1: 1rem;
+
+
+    --font-size-h2: 1.953rem;
+  }
+
+
+  body {
+    font-family: var(--font-general);
+    font-weight: var(--font-weight-general);
+    line-height: var(--line-height-general);
+    color: var(--color-font-general);
+    margin: 0;
+    padding: 0;
+    background-color: var(--color-bg-general);
+  } */
+
+  .wip {
+    border: 5px dotted red;
+  }
+
+  width: 95vw;
+  max-width: 1600px;
+  margin: auto;
+  background-color: var(--color-bg-general);
+`;
