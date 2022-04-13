4e2ae9ff26dc32db5efa98fbdbea18335bd7164d
Amar <amar@uub.localdomain>
2022-04-13 11:08:33 +0200

    import react-responsive-carousel

--- frontend/src/components/CarrousselNews/index.jsx
+++ frontend/src/components/CarrousselNews/index.jsx
@@ -1,9 +1,19 @@
-import SCarrousselNews from "./style";
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
-export default function CarrousselNews() {
+function carr() {
   return (
-    <SCarrousselNews>
-      <p>Ici le carroussel de news</p>
-    </SCarrousselNews>
+    <Carousel>
+      <div>slide</div>
+      <div>slide</div>
+      <div>slide</div>
+      <div>slide</div>
+      <div>slide</div>
+      <div>slide</div>
+      <div>slide</div>
+    </Carousel>
   );
 }
+
+export default carr;
