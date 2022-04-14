87deb6aa337942ba12e7c5fd1959a7e7e0c8c7b4
Dash293 <100704568+Dash293@users.noreply.github.com>
2022-04-14 14:36:25 +0200

    class content and syntax correction
    We have rewritten some syntax errors like class names do not using camelCase and removed double class selectors.

--- frontend/src/components/Header/style.jsx
+++ frontend/src/components/Header/style.jsx
@@ -9,7 +9,7 @@ export default styled.header`
   a {
     text-decoration: none;
   }
-  .navbar {
+  .navBar {
     height: 20vh;
     background-color: black;
     color: #fff;
@@ -23,28 +23,22 @@ export default styled.header`
     width: 100vw;
     min-height: 75px;
   }
-  .navbarsolid {
+  .navBarSolid {
     background-color: green;
     color: yellow;
   }
-  .navbar_logo {
+  .navBarLogo {
     justify-self: center;
   }
-  .navbar_links {
-    display: flex;
-  }
-  .navbar_link {
-    padding: 0 0.3rem;
-    color: inherit;
-  }
-  .navbar_burger {
+  .navBarBurger {
     width: 40px;
     height: 40px;
     background: transparent;
     border: none;
     color: inherit;
   }
-  .navbar_links {
+  .navBarLinks {
+    display: flex;
     flex-direction: column;
     justify-content: center;
     position: fixed;
@@ -57,13 +51,11 @@ export default styled.header`
     visibility: hidden;
     background-color: black;
     transition: all 0.8s ease-out;
-  }
-  .show-nav .navbar_links {
     left: 0;
     width: 30vw;
     visibility: visible;
   }
-  .navbar_item::after {
+  .navBarItem::after {
     content: "";
     display: block;
     margin: 0 auto;
@@ -71,23 +63,25 @@ export default styled.header`
     height: 1px;
     background: #fff;
   }
-  .navbar_link {
+  .navBarLink {
     display: block;
     padding: 1.5rem;
     font-size: 3vw;
+    padding: 0 0.3rem;
+    color: inherit;
   }
-  .navbar_item:last-child::after {
+  .navBarItem:last-child::after {
     display: none;
   }
-  .navbar_burger {
+  .navBarBurger {
     display: block;
   }
-  .navbar_burger:hover {
+  .navBarBurger:hover {
     cursor: pointer;
   }
-  .burger-bar,
-  .burger-bar::before,
-  .burger-bar::after {
+  .burgerBar,
+  .burgerBar::before,
+  .burgerBar::after {
     display: block;
     width: 40px;
     height: 3px;
@@ -96,49 +90,49 @@ export default styled.header`
     background-color: #fff;
     transition: all 0.5s ease-in-out;
   }
-  .burger-bar::before,
-  .burger-bar::after {
+  .burgerBar::before,
+  .burgerBar::after {
     content: "";
     position: absolute;
     left: 0;
   }
-  .burger-bar::before {
+  .burgerBar::before {
     transform: translateY(-12px);
   }
-  .burger-bar::after {
+  .burgerBar::after {
     transform: translateY(12px);
   }
-  .show-nav .burger-bar {
+  .showNav .burgerBar {
     width: 0;
     background: transparent;
   }
-  .show-nav .burger-bar::before {
+  .showNav .burgerBar::before {
     transform: rotate(45deg);
     background-color: red;
   }
-  .show-nav .burger-bar::after {
+  .showNav .burgerBar::after {
     transform: rotate(-45deg);
     background-color: red;
   }
-  .navbar_item {
+  .navBarItem {
     transform: translateY(100vh);
   }
-  .show-nav .navbar_item {
+  .showNav .navBarItem {
     transform: translateY(0);
   }
-  .show-nav .slideInDown-1 {
+  .showNav .slideInDown1 {
     transition: all 1s ease-out;
   }
-  .show-nav .slideInDown-2 {
+  .showNav .slideInDown2 {
     transition: all 1.1s ease-out;
   }
   @media screen and (max-width: 767px) {
-    .show-nav .navbar_links {
+    .showNav .navBarLinks {
       left: 0;
       width: 100vw;
       visibility: visible;
     }
-    .navbar_link {
+    .navBarLink {
       display: block;
       padding: 1.5rem;
       font-size: 6vw;
