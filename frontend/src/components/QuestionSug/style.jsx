a965854eeec6c8d632e7a93bb76c14234fc69761
[Lille-JS] Vaentin Nav <riverpure59@hotmail.fr>
2022-04-20 15:37:00 +0200

    create suggestion's page and style and add the components to the suggestion's page

---
+++ frontend/src/components/QuestionSug/style.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export default styled.div`
+  div {
+    text-align: center;
+  }
+
+  h1,
+  h2,
+  h3 {
+    text-align: center;
+    padding-top: 1rem;
+  }
+
+  .smileyImg {
+    display: flex;
+    padding: 2rem;
+    width: auto;
+    height: 125px;
+    justify-content: space-around;
+  }
+`;
