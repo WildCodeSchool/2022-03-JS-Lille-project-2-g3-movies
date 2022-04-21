96aef32d7bb48fe42383daa8102282fbadf34f4e
NtGZeko <matheooz@live.fr>
2022-04-21 22:32:18 +0200

    add button

--- frontend/src/components/modal/index.jsx
+++ frontend/src/components/modal/index.jsx
@@ -1,5 +1,18 @@
 import SModal from "./style";
 
 export default function Modal() {
-  return <SModal />;
+  return (
+    <SModal>
+      <div className="Modal">
+        <button type="button">X</button>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita cum
+          eligendi maxime necessitatibus consequatur veritatis vitae ea esse
+          sapiente consectetur aliquam aut possimus inventore laborum corrupti,
+          accusamus vero libero. Voluptatum enim assumenda sapiente suscipit
+          dolore. Adipisci odit laboriosam rerum odio!
+        </p>
+      </div>
+    </SModal>
+  );
 }
--- frontend/src/components/modal/style.jsx
+++ frontend/src/components/modal/style.jsx
@@ -1,61 +1,18 @@
 import styled from "styled-components";
 
-export default styled.modal`
-  /* .modal-btn {
-    padding: 10px 14px;
-    font-size: 18px;
-    margin: 100px auto;
-    display: block;
-    min-width: 150px;
-    cursor: pointer;
-  }
-  .modal-container {
-    position: fixed;
-    top: 0;
-    width: 100vw;
-    height: 100vh;
-    display: none;
-  }
-  .modal-container.active {
-    display: block;
-  }
-  .overlay {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: #333333d3;
-  }
-  .modal {
-    width: 95%;
-    max-width: 500px;
-    min-width: 300px;
-    padding: 30px;
-    background: #fff;
-    border-radius: 5px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-  .close-modal {
-    padding: 8px 10px;
-    border: none;
-    border-radius: 5px;
-    font-size: 18px;
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    cursor: pointer;
-    background-color: var(--color-button);
-    color: #fff;
-  }
-  .modal h1 {
-    margin-bottom: 10px;
-    font-weight: 500;
-  }
-  .modal p {
-    line-height: 1.4;
-    margin-bottom: 5px;
-    color: black; */
+export default styled.div`
+  .Modal {
+    background-color: white;
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 1px black;
+    padding: 16px;
+    width: 300px;
+    box-sizing: border-box;
+    transition: all 0.3s ease-out;
+    margin: auto;
+    margin-top: 350px;
+    transform: ${(props) =>
+      props.visible ? "translateY(0vh)" : "translateY(-100vh)"};
+    opacity: ${(props) => (props.visible ? "1" : "0")};
   }
 `;
