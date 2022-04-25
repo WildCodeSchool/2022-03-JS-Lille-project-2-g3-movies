23ba41213fdcde5c388141a8534f07ee21680a32
NtGZeko <matheooz@live.fr>
2022-04-25 09:53:36 +0200

    correctif

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -5,6 +5,10 @@ import Modal3 from "../Modal3";
 
 export default function Footer() {
   return (
+    /* J'ai créé 3 modules de modal pour que les 3 modal agissent indépendamment 
+    dans un 1er temps j'ai voulu mettre les 3 modale dans le même composent mais 
+    les 3 étaient "connectés"  tous les boutons ouvrés 
+    qu'une seule modale chose qu'on ne veut pas d'où la création de 3 modules */
     <SFooter>
       <Modal />
       <Modal2 />
