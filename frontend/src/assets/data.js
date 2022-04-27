e665b11c5ae6b7cc35caa84693aa1fe26adebd4b
Ilalande <jcasier@protonmail.com>
2022-04-27 15:28:00 +0200

    Putting a map and changing the way to export data.js for CarouselType in home

--- frontend/src/assets/data.js
+++ frontend/src/assets/data.js
@@ -10,68 +10,60 @@ import imgRomance from "./romance.jpg";
 import imgSciFi from "./sciFi.jpg";
 import imgWestern from "./western.jpg";
 
-export const action = {
-  key: "action",
-  picture: imgAction,
-  alt: "Link to action movies",
-};
-
-export const comedy = {
-  key: "comedy",
-  picture: imgComedy,
-  alt: "Link to comedy movies",
-};
-
-export const crime = {
-  key: "crime",
-  picture: imgCrime,
-  alt: "Link to crime movies",
-};
-
-export const documentary = {
-  key: "documentary",
-  picture: imgDocumentary,
-  alt: "Link to documentary movies",
-};
-
-export const drama = {
-  key: "drama",
-  picture: imgDrama,
-  alt: "Link to drama movies",
-};
-
-export const family = {
-  key: "family",
-  picture: imgFamily,
-  alt: "Link to family movies",
-};
-
-export const fantasy = {
-  key: "fantasy",
-  picture: imgFantasy,
-  alt: "Link to fantasy movies",
-};
-
-export const horror = {
-  key: "horror",
-  picture: imgHorror,
-  alt: "Link to horror movies",
-};
-
-export const romance = {
-  key: "romance",
-  picture: imgRomance,
-  alt: "Link to romance movies",
-};
-
-export const sciFi = {
-  key: "sciFi",
-  picture: imgSciFi,
-  alt: "Link to Sci-Fi movies",
-};
-
-export const western = {
-  key: "western",
-  picture: imgWestern,
-  alt: "Link to western movies",
-};
+export default [
+  {
+    key: "action",
+    picture: imgAction,
+    alt: "Link to action movies",
+  },
+  {
+    key: "comedy",
+    picture: imgComedy,
+    alt: "Link to comedy movies",
+  },
+  {
+    key: "crime",
+    picture: imgCrime,
+    alt: "Link to crime movies",
+  },
+  {
+    key: "documentary",
+    picture: imgDocumentary,
+    alt: "Link to documentary movies",
+  },
+  {
+    key: "drama",
+    picture: imgDrama,
+    alt: "Link to drama movies",
+  },
+  {
+    key: "family",
+    picture: imgFamily,
+    alt: "Link to family movies",
+  },
+  {
+    key: "fantasy",
+    picture: imgFantasy,
+    alt: "Link to fantasy movies",
+  },
+  {
+    key: "horror",
+    picture: imgHorror,
+    alt: "Link to horror movies",
+  },
+  {
+    key: "romance",
+    picture: imgRomance,
+    alt: "Link to romance movies",
+  },
+  {
+    key: "sciFi",
+    picture: imgSciFi,
+    alt: "Link to Sci-Fi movies",
+  },
+  {
+    key: "western",
+    picture: imgWestern,
+    alt: "Link to western movies",
+  },
+];
--- frontend/src/components/TypeCarroussel/index.jsx
+++ frontend/src/components/TypeCarroussel/index.jsx
@@ -1,19 +1,7 @@
 import { Link } from "react-router-dom";
 /* eslint-disable import/no-unresolved */
 import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  action,
-  comedy,
-  crime,
-  documentary,
-  drama,
-  family,
-  fantasy,
-  horror,
-  romance,
-  sciFi,
-  western,
-} from "../../assets/data";
+import genres from "../../assets/data";
 import "swiper/css";
 import "swiper/css/navigation";
 import STypeCarroussel from "./style";
@@ -24,62 +12,15 @@ export default function TypeCarroussel() {
   return (
     <STypeCarroussel>
       <Swiper slidesPerView={3} spaceBetween={10} className="mySwiper">
-        <SwiperSlide>
-          <Link to={`/results/${action.key}`}>
-            <img src={action.picture} alt={action.alt} />
-          </Link>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <Link to={`/results/${comedy.key}`}>
-            <img src={comedy.picture} alt={comedy.alt} />
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link to={`/results/${crime.key}`}>
-            <img src={crime.picture} alt={crime.alt} />
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link to={`/results/${documentary.key}`}>
-            <img src={documentary.picture} alt={documentary.alt} />
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link to={`/results/${drama.key}`}>
-            <img src={drama.picture} alt={drama.alt} />
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link to={`/results/${family.key}`}>
-            <img src={family.picture} alt={family.alt} />
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link to={`/results/${fantasy.key}`}>
-            <img src={fantasy.picture} alt={fantasy.alt} />
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link to={`/results/${horror.key}`}>
-            <img src={horror.picture} alt={horror.alt} />
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link to={`/results/${romance.key}`}>
-            <img src={romance.picture} alt={romance.alt} />
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link to={`/results/${sciFi.key}`}>
-            <img src={sciFi.picture} alt={sciFi.alt} />
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Link to={`/results/${western.key}`}>
-            <img src={western.picture} alt={western.alt} />
-          </Link>
-        </SwiperSlide>
+        {genres.map((genre) => {
+          return (
+            <SwiperSlide>
+              <Link to={`/results/${genre.key}`}>
+                <img src={genre.picture} alt={genre.alt} />
+              </Link>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </STypeCarroussel>
   );
