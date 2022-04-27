ce2340b370f638a706d5db5881e11d1bbe42ecb5
ilalande <86239514+ilalande@users.noreply.github.com>
2022-04-27 16:55:38 +0200

    Update index.jsx

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -5,10 +5,7 @@ import Modal3 from "../Modal3";
 
 export default function Footer() {
   return (
-    /* J'ai créé 3 modules de modal pour que les 3 modal agissent indépendamment 
-    dans un 1er temps j'ai voulu mettre les 3 modale dans le même composent mais 
-    les 3 étaient "connectés"  tous les boutons ouvrés 
-    qu'une seule modale chose qu'on ne veut pas d'où la création de 3 modules */
+    /* 3 modals are created in order to change the text inside (the easiest way to do it / Done with Luigi. To imprive in a next release */
     <SFooter>
       <Modal />
       <Modal2 />
