3f3f712e744e20bb6b77a80a54d1661de8c768a4
Ilalande <jcasier@protonmail.com>
2022-04-27 15:07:19 +0200

    giving up setSearchParams

--- frontend/src/components/SugResult/index.jsx
+++ frontend/src/components/SugResult/index.jsx
@@ -4,12 +4,11 @@ import axios from "axios";
 import SSugResult from "./style";
 
 export default function SugResult() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
     const request = searchParams.toString();
-
     axios
       .get(
         `https://api.themoviedb.org/3/discover/movie?api_key=923a42c07dc9afdc9a05b46fb2fd558b&${request}`
