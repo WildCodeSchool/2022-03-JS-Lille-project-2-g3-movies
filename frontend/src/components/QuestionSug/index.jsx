74d4da12bcf83dccd482362b78236c0911b30d30
Ilalande <jcasier@protonmail.com>
2022-04-28 10:46:30 +0200

    Creating class to make a link on pictures. Creating datasuggest in order to map the buttons. Adjusting css

--- frontend/src/components/QuestionSug/index.jsx
+++ frontend/src/components/QuestionSug/index.jsx
@@ -27,31 +27,40 @@ export default function QuestionSug() {
                 </button>
               ))}
           </section>
-          <h3> Whats your movie style ?</h3>
+          <h3> What is your movie style ?</h3>
           <section className="smileyImg">
-            <button type="button" className="Img" onClick={handleChoiceLinks}>
-              <img src="src/assets/old.png" alt="80's" />
-            </button>
-            <button type="button" className="Img" onClick={handleChoiceLinks}>
-              <img src="src/assets/young.png" alt="2000's" />
-            </button>
-            <button type="button" className="Img" onClick={handleChoiceLinks}>
-              <img src="src/assets/baby.png" alt="actual" />
-            </button>
+            {smileys
+              .filter((smiley) => smiley.type.includes("dateRelease"))
+              .map((smiley) => (
+                <button
+                  type="button"
+                  className="Img"
+                  onClick={handleChoiceLinks}
+                >
+                  <img src={smiley.picture} alt={smiley.alt} />
+                </button>
+              ))}
           </section>
-          <h3>Whats your condition?</h3>
+          <h3> What is your condition ?</h3>
           <section className="smileyImg">
-            <button type="button" className="Img" onClick={handleChoiceLinks}>
-              <img src="src/assets/low-energy.png" alt="under 1h30" />
-            </button>
-            <button type="button" className="Img" onClick={handleChoiceLinks}>
-              <img className="" src="src/assets/energy.png" alt="plus 1h30" />
-            </button>
+            {smileys
+              .filter((smiley) => smiley.type.includes("runtime"))
+              .map((smiley) => (
+                <button
+                  type="button"
+                  className="Img"
+                  onClick={handleChoiceLinks}
+                >
+                  <img src={smiley.picture} alt={smiley.alt} />
+                </button>
+              ))}
           </section>
         </div>
-        <button type="button" className="button">
-          Show your result
-        </button>
+        <div className="buttonResults">
+          <button type="button" className="button">
+            Show your result
+          </button>
+        </div>
       </div>
     </SQuestionSug>
   );
