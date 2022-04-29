e7bd5e81f46c2c62dc0db381e33716dcd176d2ed
NtGZeko <matheooz@live.fr>
2022-04-29 11:38:15 +0200

    fix eslint rules errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
+const path = require("path");
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
