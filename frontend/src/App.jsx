9d3e71e36cdd1e7a7498a7681c24746058865bf0
Ilalande <jcasier@protonmail.com>
2022-05-03 12:24:47 +0200

    Deletting useless page Suggestion to link to the component directly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import ResultsCategory from "@components/ResultsCategory";
 import Home from "./pages/Home";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Suggestion from "./pages/Suggestion";
+import QuestionSug from "./components/QuestionSug";
 import SGlobal from "./style";
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/suggestion" element={<Suggestion />} />
+        <Route path="/suggestion" element={<QuestionSug />} />
         <Route path="/results/:category" element={<ResultsCategory />} />
       </Routes>
       <Footer />
