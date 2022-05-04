d2494d90842d4f6aa3da98760ff05bba504c949f
Ilalande <jcasier@protonmail.com>
2022-05-04 10:24:46 +0200

    Deleting useless page suggestion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import ResultsWeekly from "@components/ResultsWeekly";
+import QuestionSug from "@components/QuestionSug";
 import ResultsCategory from "./components/ResultsCategory";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Suggestion from "./pages/Suggestion";
 import SGlobal from "./style";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/suggestion" element={<Suggestion />} />
+        <Route path="/suggestion" element={<QuestionSug />} />
         <Route path="/results/:category" element={<ResultsCategory />} />
         <Route path="/weeklyupcoming" element={<ResultsWeekly />} />
       </Routes>
